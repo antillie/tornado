653b43a5656244c3c9088b85b94c286716b75b99
George Markeloff <george.markeloff@rackspace.com>
2017-11-02 14:56:58 -0500

    looking better

--- sites/tun0.com/js/register.js
+++ sites/tun0.com/js/register.js
@@ -4,37 +4,36 @@ function create_user() {
     
     var pwd = document.getElementById("pwd").value;
     var pwd2 = document.getElementById("pwd2").value;
+    var name = document.getElementById("usr").value;
     
+    if (name.length < 3) {
+        display_error("name_short_error");
+        return;
+    };
     if (pwd != pwd2) {        
         display_error("match_error");
         return;
     };
-    
     if (pwd.length < 10) {
-        
-        // display a message about the password being too short
-        
+        display_error("pw_short_error");
         return;
     };
     
     var request_object = new Object();
     request_object.user = document.getElementById("usr").value;
     request_object.password = document.getElementById("pwd").value;
-    request_object.captcha = $("#g-recaptcha-response").val()
-    api_call("/register", request_object, process_register)
+    request_object.captcha = $("#g-recaptcha-response").val();
+    api_call("/register", request_object, process_register);
 };
 
 function process_register(api_response) {
     
-    var data = JSON.parse(api_response)
-    console.log(data)
+    var data = JSON.parse(api_response);
     
     if (data["success"]) {
-        console.log("sucess")
         send_to_page("");
     }
     else {
-        console.log("failure")
+        display_error("name_error");
     };
-
 };
