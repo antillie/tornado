a06eb09f1f47c4e713482f2b2ff64a0b4e67a647
George Markeloff <george.markeloff@rackspace.com>
2017-11-02 17:17:22 -0500

    trying phaser

--- sites/tun0.com/js/index.js
+++ sites/tun0.com/js/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var game = null;
+
 function get_user() {
     api_call("/get_user", null, show_user)
 };
@@ -13,5 +15,28 @@ function show_user(api_response) {
     }
     else {
         sessionStorage.setItem("user", data.user);
+        run_game();
     };
 };
+
+function run_game() {
+    
+    game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game_container', { preload: preload, create: create });
+    
+};
+
+
+
+
+function preload () {
+
+    game.load.image('logo', '/hellophaser/phaser.png');
+
+}
+
+function create () {
+
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
+
+}
