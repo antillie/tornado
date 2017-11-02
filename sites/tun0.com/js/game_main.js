0c3d7174ede969b0029c226611c80fe773fba5b5
George Markeloff <george.markeloff@rackspace.com>
2017-11-02 17:43:53 -0500

    canvas for 2d

--- sites/tun0.com/js/game_main.js
+++ sites/tun0.com/js/game_main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function run_game() {
-    game = new Phaser.Game(1280, 720, Phaser.AUTO, 'phaser_box', { preload: preload, create: create });
+    game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'phaser_box', { preload: preload, create: create, update: update });
 };
 
 function preload () {
@@ -11,4 +11,8 @@ function preload () {
 function create () {
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
+};
+
+function update() {
+    return;
 };
