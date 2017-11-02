0fa442d80bd7b3f5978b47251470f5495b98b389
George Markeloff <george.markeloff@rackspace.com>
2017-11-02 16:30:55 -0500

    login polish

--- sites/tun0.com/js/login.js
+++ sites/tun0.com/js/login.js
@@ -1,24 +1,38 @@
 "use strict";
 
+var captcha_checked = false;
+
 function do_login() {
+    
+    var pwd = document.getElementById("pwd").value;
+    var name = document.getElementById("usr").value;
+    
+    if (!captcha_checked) {
+        display_error("captcha_error");
+        return;
+    };
+    
     var request_object = new Object();
-    request_object.user = document.getElementById("usr").value;
-    request_object.password = document.getElementById("pwd").value;
-    request_object.captcha = $("#g-recaptcha-response").val()
-    api_call("/login", request_object, process_login)
+    request_object.user = name;
+    request_object.password = pwd;
+    request_object.captcha = $("#g-recaptcha-response").val();
+    api_call("/login", request_object, process_login);
 };
 
 function process_login(api_response) {
     
     var data = JSON.parse(api_response)
-    console.log(data)
     
     if (data["login"]) {
-        console.log("sucess")
         send_to_page("");
     }
     else {
-        console.log("failure")
+        display_error("login_error");
+        grecaptcha.reset();
+        captcha_checked = false;
     };
+};
 
+function recaptcha_checked() {
+    captcha_checked = true;
 };
