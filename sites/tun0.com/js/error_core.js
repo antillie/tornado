c354eec9eb1011b94e9be74c8955d8bab82e1188
George Markeloff <george.markeloff@rackspace.com>
2017-11-02 14:41:40 -0500

    cleaner

--- sites/tun0.com/js/error_core.js
+++ sites/tun0.com/js/error_core.js
@@ -4,7 +4,6 @@ function display_error(error_id, auto_clear=true) {
     var error = document.getElementById(error_id);
     error.classList.remove("hide")
     error.style.opacity = 1;
-    error.style.display = "block";
     if (auto_clear) {
         setTimeout(function(){ fade(error); }, 3000);
     };
@@ -15,7 +14,7 @@ function fade(element) {
     var timer = setInterval(function () {
         if (op <= 0.1){
             clearInterval(timer);
-            element.style.display = 'none';
+            element.classList.add("hide")
         }
         element.style.opacity = op;
         op -= op * 0.1;
@@ -24,6 +23,5 @@ function fade(element) {
 
 function hide_error(error_id) {
     var error = document.getElementById(error_id);
-    //error.style.display = "none";
     error.classList.add("hide")
 };
