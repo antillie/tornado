534e9facf64717b8bd3e56f0052d15fbed3e279e
antillie <antillie@gmail.com>
2017-11-02 20:18:02 -0500

    ?

--- sites/tun0.com/js/game_main.js
+++ sites/tun0.com/js/game_main.js
@@ -15,22 +15,22 @@ var bootState = {
         game.load.image('logo', '/images/phaser.png');
         var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
-    };
+    }
     
 };
 
-function preload () {
-    game.load.image('logo', '/images/phaser.png');
-};
-
-function create () {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
-};
-
-function update() {
-    return;
-};
+//function preload () {
+//    game.load.image('logo', '/images/phaser.png');
+//};
+//
+//function create () {
+//    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+//    logo.anchor.setTo(0.5, 0.5);
+//};
+//
+//function update() {
+//    return;
+//};
 
 // https://github.com/kiswa/phaser-template/blob/master/src/js/init.js
 // https://github.com/MattMcFarland/phaser-menu-system
