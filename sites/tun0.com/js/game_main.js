3bb463551e0601c830190852ed7af69f46d497b2
antillie <antillie@gmail.com>
2017-11-02 20:57:13 -0500

    state functions

--- sites/tun0.com/js/game_main.js
+++ sites/tun0.com/js/game_main.js
@@ -10,16 +10,27 @@ function run_game() {
 
 var bootState = {
     
-    create: function () {
-        
-        //game.physics.startSystem(Phaser.Physics.ARCADE)
+    preload: function () {
         game.load.image('logo', '/images/phaser.png');
-        
         game.load.start()
-        
+    }
+    
+    loadUpdate: function () {
+        return;
+    }
+    
+    create: function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE)
+        return;
+    }
+    
+    update: function () {
         game.state.start("load");
     }
     
+    shutdown: function () {
+        return;
+    }
 };
 
 var loadState = {
@@ -35,4 +46,4 @@ var loadState = {
 // https://github.com/kiswa/phaser-template/blob/master/src/js/init.js
 // https://github.com/MattMcFarland/phaser-menu-system
 // http://perplexingtech.weebly.com/game-dev-blog/using-states-in-phaserjs-javascript-game-developement
-// http://www.html5gamedevs.com/topic/4225-how-do-you-use-a-webfont-in-phaser-exactly/
+// http://www.html5gamedevs.com/topic/4225-how-do-you-use-a-webfont-in-phaser-exactly/````````````````````
