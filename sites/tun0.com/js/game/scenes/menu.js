155d2e47eb27ecea4cc42d3a47e63876eb71a08d
George Markeloff <george.markeloff@rackspace.com>
2017-11-03 16:05:20 -0500

    .

--- sites/tun0.com/js/game/scenes/menu.js
+++ sites/tun0.com/js/game/scenes/menu.js
@@ -14,13 +14,13 @@ var menuState = {
     
     create: function () {
         
-        var entry1 = game.add.text(game.world.centerX, game.world.centerY - 100, "New Game", { font: "20px immortal", fill: "#f0dc00", align: "center" });
-        var entry2 = game.add.text(game.world.centerX, game.world.centerY - 70, "Load Game", { font: "20px immortal", fill: "#ffffff", align: "center" });
-        var entry3 = game.add.text(game.world.centerX, game.world.centerY - 40, "Credits", { font: "20px immortal", fill: "#ffffff", align: "center" });
+        this.entry1 = game.add.text(game.world.centerX, game.world.centerY - 100, "New Game", { font: "20px immortal", fill: "#f0dc00", align: "center" });
+        this.entry2 = game.add.text(game.world.centerX, game.world.centerY - 70, "Load Game", { font: "20px immortal", fill: "#ffffff", align: "center" });
+        this.entry3 = game.add.text(game.world.centerX, game.world.centerY - 40, "Credits", { font: "20px immortal", fill: "#ffffff", align: "center" });
         
-        entry1.anchor.set(0.5, 0.5);
-        entry2.anchor.set(0.5, 0.5);
-        entry3.anchor.set(0.5, 0.5);
+        this.entry1.anchor.set(0.5, 0.5);
+        this.entry2.anchor.set(0.5, 0.5);
+        this.entry3.anchor.set(0.5, 0.5);
         
         music = game.add.audio("title_loop");
         
