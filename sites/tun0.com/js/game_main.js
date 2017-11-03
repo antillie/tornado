498795197e75e6d28856bb1aee2bf5f80a9e40ce
antillie <antillie@gmail.com>
2017-11-02 22:31:24 -0500

    .

--- sites/tun0.com/js/game_main.js
+++ sites/tun0.com/js/game_main.js
@@ -11,27 +11,16 @@ function run_game() {
 var bootState = {
     
     preload: function () {
-        game.load.image('logo', '/images/phaser.png');
         game.load.image('game_logo', '/images/site_icon.png');
-        game.load.audio("title_loop", "/sound/music/enchantedfestivalloop.mp3")
         //game.load.onFileComplete.add(update_progress_bar, this);
-        
-        this.preloadBar = game.add.graphics(0, 50);
-        this.preloadBar.lineStyle(3, 0xffffff, 1);
-        this.preloadBar.moveTo(0, 0);
-        this.preloadBar.lineTo(game.width, 0);
-        this.preloadBar.scale.x = 0;
-        
         game.load.start();
     },
     
     loadUpdate: function () {
-        this.preloadBar.scale.x = game.load.progress * 0.01;
         return;
     },
     
     create: function () {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
         return;
     },
     
@@ -47,16 +36,25 @@ var bootState = {
 var loadState = {
     
     preload: function () {
-        return;
+        game.load.audio("menu_change", "/sound/menu_change.wav")
+        game.load.audio("title_loop", "/sound/music/enchantedfestivalloop.mp3")
+        this.preloadBar = game.add.graphics(0, 50);
+        this.preloadBar.lineStyle(3, 0xffffff, 1);
+        this.preloadBar.moveTo(0, 0);
+        this.preloadBar.lineTo(game.width, 0);
+        this.preloadBar.scale.x = 0;
+        game.load.start();
     },
     
     loadUpdate: function () {
-        return;
+        this.preloadBar.scale.x = game.load.progress * 0.01;
     },
     
     create: function () {
         
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'game_logo');
         logo.anchor.setTo(0.5, 0.5);
     }
     
@@ -73,4 +71,4 @@ var loadState = {
 // https://github.com/kiswa/phaser-template/blob/master/src/js/init.js
 // https://github.com/MattMcFarland/phaser-menu-system
 // http://perplexingtech.weebly.com/game-dev-blog/using-states-in-phaserjs-javascript-game-developement
-// http://www.html5gamedevs.com/topic/4225-how-do-you-use-a-webfont-in-phaser-exactly/````````````````````
+// http://www.html5gamedevs.com/topic/4225-how-do-you-use-a-webfont-in-phaser-exactly/
