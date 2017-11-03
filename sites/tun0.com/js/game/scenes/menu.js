a777a247c5e4a465db1b52e1fe1c0ecdb20f91e5
George Markeloff <george.markeloff@rackspace.com>
2017-11-03 00:20:25 -0500

    menu skeleton

--- sites/tun0.com/js/game/scenes/menu.js
+++ sites/tun0.com/js/game/scenes/menu.js
@@ -13,8 +13,8 @@ var menuState = {
     create: function () {
         
         var entry1 = game.add.text(game.world.centerX, game.world.centerY - 100, "New Game", { font: "20px immortal", fill: "#f0dc00", align: "center" });
-        var entry2 = game.add.text(game.world.centerX, game.world.centerY - 60, "Load Game", { font: "20px immortal", fill: "#ffffff", align: "center" });
-        var entry3 = game.add.text(game.world.centerX, game.world.centerY - 20, "New Game", { font: "20px immortal", fill: "#ffffff", align: "center" });
+        var entry2 = game.add.text(game.world.centerX, game.world.centerY - 70, "Load Game", { font: "20px immortal", fill: "#ffffff", align: "center" });
+        var entry3 = game.add.text(game.world.centerX, game.world.centerY - 40, "Credits", { font: "20px immortal", fill: "#ffffff", align: "center" });
         
         entry1.anchor.set(0.5, 0.5)
         entry2.anchor.set(0.5, 0.5)
