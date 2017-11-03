3a2d92ac9361b81b05011a82994bb74c62c669db
antillie <antillie@gmail.com>
2017-11-02 20:28:57 -0500

    states work

--- sites/tun0.com/js/game_main.js
+++ sites/tun0.com/js/game_main.js
@@ -32,18 +32,7 @@ var loadState = {
     
 };
 
-//function preload () {
-//    game.load.image('logo', '/images/phaser.png');
-//};
-//
-//function create () {
-//    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-//    logo.anchor.setTo(0.5, 0.5);
-//};
-//
-//function update() {
-//    return;
-//};
-
 // https://github.com/kiswa/phaser-template/blob/master/src/js/init.js
 // https://github.com/MattMcFarland/phaser-menu-system
+// http://perplexingtech.weebly.com/game-dev-blog/using-states-in-phaserjs-javascript-game-developement
+// http://www.html5gamedevs.com/topic/4225-how-do-you-use-a-webfont-in-phaser-exactly/
