930d7be29d0e7e6c8b425c4245a4c59b90429724
antillie <antillie@gmail.com>
2017-11-02 20:16:12 -0500

    state?

--- sites/tun0.com/js/game_main.js
+++ sites/tun0.com/js/game_main.js
@@ -1,7 +1,22 @@
 "use strict";
 
 function run_game() {
-    game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'phaser_box', { preload: preload, create: create, update: update });
+    game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'phaser_box');
+    game.state.add("boot", bootState);
+    
+    game.state.start("boot");
+};
+
+var bootState = {
+    
+    create: function () {
+        
+        //game.physics.startSystem(Phaser.Physics.ARCADE)
+        game.load.image('logo', '/images/phaser.png');
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    
 };
 
 function preload () {
