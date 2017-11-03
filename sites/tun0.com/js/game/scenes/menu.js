73bda3d0aa8df3d54110819d68aab13ac3dfc119
George Markeloff <george.markeloff@rackspace.com>
2017-11-03 00:16:59 -0500

    .

--- sites/tun0.com/js/game/scenes/menu.js
+++ sites/tun0.com/js/game/scenes/menu.js
@@ -13,7 +13,7 @@ var menuState = {
     create: function () {
         
         var entry1 = game.add.text(game.world.centerX, game.world.centerY - 100, "New Game", { font: "20px immortal", fill: "#f0dc00", align: "center" });
-        text.anchor.set(0.5, 0.5)
+        entry1.anchor.set(0.5, 0.5)
         
     },
     
