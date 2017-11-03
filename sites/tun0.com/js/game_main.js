3e47f21376883c598af9b70ece6bed1da8f800d7
antillie <antillie@gmail.com>
2017-11-02 21:42:00 -0500

    sound files

--- sites/tun0.com/js/game_main.js
+++ sites/tun0.com/js/game_main.js
@@ -15,11 +15,11 @@ var bootState = {
         game.load.image('game_logo', '/images/site_icon.png');
         game.load.onFileComplete.add(update_progress_bar, this);
         
-        this.preloadBar = game.add.graphics(0, 50);
-        this.preloadBar.lineStyle(3, 0xffffff, 1);
-        this.preloadBar.moveTo(0, 0);
-        this.preloadBar.lineTo(game.width, 0);
-        this.preloadBar.scale.x = 0; // set the bar to the beginning position
+        //this.preloadBar = game.add.graphics(0, 50);
+        //this.preloadBar.lineStyle(3, 0xffffff, 1);
+        //this.preloadBar.moveTo(0, 0);
+        //this.preloadBar.lineTo(game.width, 0);
+        //this.preloadBar.scale.x = 0; // set the bar to the beginning position
         
         // every frame during loading, set the scale.x of the bar to the progress (an integer between 0  // and 100) divided by 100 to give a float between 0 and 1  
         
@@ -27,7 +27,7 @@ var bootState = {
     },
     
     loadUpdate: function () {
-        this.preloadBar.scale.x = game.load.progress * 0.01;
+        //this.preloadBar.scale.x = game.load.progress * 0.01;
         return;
     },
     
