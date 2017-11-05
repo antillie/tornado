78082ea1f076c11c81312b129501d7cb151b45e2
antillie <antillie@gmail.com>
2017-11-05 11:18:05 -0600

    .

--- sites/tun0.com/js/game/scenes/initial.js
+++ sites/tun0.com/js/game/scenes/initial.js
@@ -71,7 +71,7 @@ var initialState = {
         this.game.physics.arcade.collide(game_data.player["sprite"], this.campfire);
         
         if (checkOverlap(game_data.player["sprite"], game_data.initial.objects["world_exit"])) {
-            console.log("hotspot hit")
+            world_exit();
         };
         
         game_data.player["sprite"].body.velocity.x = 0;
