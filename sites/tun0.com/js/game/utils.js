87cf8d30adfc57427695b773f97f9eb4519df7f9
antillie <antillie@gmail.com>
2017-11-05 09:13:00 -0600

    test

--- sites/tun0.com/js/game/utils.js
+++ sites/tun0.com/js/game/utils.js
@@ -11,6 +11,6 @@ function make_objects(scene_name, object_list) {
         game_data[scene_name]["objects"][object_list[i].properties.id] = game.add.sprite(object_list[i].x, object_list[i].y, null)
         game_data[scene_name]["objects"][object_list[i].properties.id].enableBody = true;
         game.physics.arcade.enable(game_data[scene_name]["objects"][object_list[i].properties.id]);
-        game_data[scene_name]["objects"][object_list[i].properties.id].body.setSize(object_list[i].width, object_list[i].height, 0, 0);
+        //game_data[scene_name]["objects"][object_list[i].properties.id].body.setSize(object_list[i].width, object_list[i].height, 0, 0);
     };
 };
