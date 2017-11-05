f2e32adde9163b740b8bde0ddb67ad0870527ef6
antillie <antillie@gmail.com>
2017-11-05 12:28:04 -0600

    .

--- sites/tun0.com/js/game/utils.js
+++ sites/tun0.com/js/game/utils.js
@@ -9,6 +9,7 @@ function checkOverlap(spriteA, spriteB) {
 function make_objects(scene_name, object_list) {
     for (var i = 0; i < object_list.length; i++) {
         game_data[scene_name]["objects"][object_list[i].properties.id] = game.add.sprite(object_list[i].x, object_list[i].y, null)
+        console.log(object_list[i])
         game_data[scene_name]["objects"][object_list[i].properties.id].enableBody = true;
         game.physics.arcade.enable(game_data[scene_name]["objects"][object_list[i].properties.id]);
     };
