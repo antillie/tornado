ab6d39b1d8608652728f97f0b48b00eca6902cab
antillie <antillie@gmail.com>
2017-11-05 08:29:04 -0600

    .

--- sites/tun0.com/js/game/scenes/initial.js
+++ sites/tun0.com/js/game/scenes/initial.js
@@ -177,3 +177,12 @@ function make_objects(object_list) {
     
     
 };
+
+function checkOverlap(spriteA, spriteB) {
+
+    var boundsA = spriteA.getBounds();
+    var boundsB = spriteB.getBounds();
+
+    return Phaser.Rectangle.intersects(boundsA, boundsB);
+
+}
