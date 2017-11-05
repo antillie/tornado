2e667611260e0b49e9bcf9eb0b29375a5677b22f
antillie <antillie@gmail.com>
2017-11-05 08:34:29 -0600

    .

--- sites/tun0.com/js/game/scenes/initial.js
+++ sites/tun0.com/js/game/scenes/initial.js
@@ -30,7 +30,7 @@ var initialState = {
         
         game_data.initial["objects"] = new Object();
         
-        make_objects("initial", this.map.objects["Object Layer 1"]);
+        make_objects(this.map.objects["Object Layer 1"]);
         
         console.log(game_data.initial["objects"])
         
@@ -164,11 +164,11 @@ function world_exit() {
     console.log("called")
 };
 
-function make_objects(scene_name, object_list) {
+function make_objects(object_list) {
     for (var i = 0; i < object_list.length; i++) {
-        game_data[scene_name]objects[object_list[i].properties.id] = game.add.sprite(object_list[i].x, object_list[i].y, null)
-        game_data[scene_name]objects[object_list[i].properties.id].enableBody = true;
-        game.physics.arcade.enable(game_data[scene_name]objects[object_list[i].properties.id]);
-        game_data[scene_name]objects[object_list[i].properties.id].body.setSize(object_list[i].width, object_list[i].height, 0, 0);
+        game_data.initial.objects[object_list[i].properties.id] = game.add.sprite(object_list[i].x, object_list[i].y, null)
+        game_data.initial.objects[object_list[i].properties.id].enableBody = true;
+        game.physics.arcade.enable(game_data.initial.objects[object_list[i].properties.id]);
+        game_data.initial.objects[object_list[i].properties.id].body.setSize(object_list[i].width, object_list[i].height, 0, 0);
     };
 };
